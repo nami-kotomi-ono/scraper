9a36451430e892bea0cb69b8ca64104b7fa95d7f
nami-kotomi-ono <qinmeixiaoye68@gmail.com>
2025-04-15 12:33:30 +0900

    微修正

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import os
 import sys
-import pytest
-from app.services.file_manager import cleanup_files
 
 # プロジェクトのルートディレクトリをPythonのパスに追加
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, project_root)
+
+import pytest
+from app.services.file_manager import cleanup_files
 
 @pytest.fixture(autouse=True)
 def cleanup_test_files():
